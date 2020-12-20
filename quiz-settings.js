817e9b66b96c744925099a8ffe9ab374f07bbada
Eniserdol <eniserdol@hotmail.com>
2020-12-20 17:08:18 +0300

    Add quiz-settings.js

---
+++ quiz-settings.js
@@ -0,0 +1,8 @@
+class QuizSettings {
+  constructor(category, difficulty, numberOfQuestions) {
+    this.category = category
+    this.difficulty = difficulty
+    this.numberOfQuestions = numberOfQuestions
+  }
+}
+module.exports = QuizSettings
