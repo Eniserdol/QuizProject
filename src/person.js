245599aed2dfb83b4cf441c386510a8129aeeacb
Eniserdol <eniserdol@hotmail.com>
2021-01-08 00:28:38 +0300

    Refactor person.js add constructor parameters

--- src/person.js
+++ src/person.js
@@ -1,14 +1,30 @@
 // eslint-disable-next-line no-unused-vars
 
 class Person {
-  constructor(name, id) {
+  constructor(name, id /* password,email */) {
     this.name = name
     this.id = id
     this.answer = []
+    // add password and email as private ??
+    // #password = [];
+    // #email =[];
   }
 
   addAnswer(answer) {
     this.answer.push(answer)
   }
+  /*
+  login(userEmail,userPassword) {
+      this.#email === userEmail
+      this.#password === userPassword
+
+  }
+  */
 }
+
+const coyotiv1 = new Person('Coyotiv1', 1)
+const coyotiv2 = new Person('Coyotiv2', 2)
+
+console.log(coyotiv1.answer === coyotiv2.answer)
+
 module.exports = Person
