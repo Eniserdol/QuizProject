175ba708280b5d7eee928aa4e0cab34ea2ec21c9
Eniserdol <eniserdol@hotmail.com>
2021-01-10 23:37:07 +0300

    Refactor class constructor & add new methoods & add export module

--- src/questions.js
+++ src/questions.js
@@ -1,10 +1,29 @@
-class Question {
-  constructor(questionType, questionNumber, text, choices, answer) {
-    this.questionType = questionType
-    this.questionNumber = questionNumber
-    this.text = text
-    this.choices = choices
-    this.answer = answer
+export default class Questions {
+  constructor() {
+    this.questions = ['What is 10/5 ?', 'Who invented JavaScript?', 'What is 2*6']
+    this.answers = [`a: 3 b: 2 c: 5`, `a: Douglas Crockford,b: Sheryl Sandberg, c: Brendan Eich`, `a: 12 b: 3 c: 8`]
+    this.quizAnswers = ['b', 'c', 'a']
+  }
+
+  newQuestionAdd(questionText, answersText, correctAnswer) {
+    this.questions.push(questionText)
+    this.answers.push(answersText)
+    this.quizAnswers.push(correctAnswer)
+  }
+
+  getQuestion(index) {
+    return this.questions[index]
+  }
+
+  getAnswer(index) {
+    return this.answers[index]
+  }
+
+  getQuizAnswers(index) {
+    return this.quizAnswers[index]
+  }
+
+  getQuestionsLength() {
+    return this.questions.length
   }
 }
-module.exports = Question
