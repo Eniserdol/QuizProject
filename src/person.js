dfeba429db8fa24f3acd5420729536e29a211b2f
Eniserdol <eniserdol@hotmail.com>
2021-01-16 20:59:55 +0300

    Refactor constuctor add performance Level Add level gain property

--- src/person.js
+++ src/person.js
@@ -1,10 +1,11 @@
 // eslint-disable-next-line no-unused-vars
 
 class Person {
-  constructor(name, id /* password,email */) {
+  constructor(name, id, performanceLevel /* password,email */) {
     this.name = name
     this.id = id
     this.answer = []
+    this.performanceLevel = performanceLevel
     // add password and email as private ??
     // #password = [];
     // #email =[];
@@ -13,6 +14,7 @@ class Person {
   addAnswer(answer) {
     this.answer.push(answer)
   }
+
   /*
   login(userEmail,userPassword) {
       this.#email === userEmail
@@ -20,6 +22,18 @@ class Person {
 
   }
   */
+
+  // Level gain property is added
+
+  gainLevel() {
+    if (this.answer > 0 && this.answer < 5) {
+      this.performanceLevel = 1
+    } else if (this.answer >= 5 && this.answer < 10) {
+      this.performanceLevel = 2
+    } else {
+      this.performanceLevel = 3
+    }
+  }
 }
 
 const coyotiv1 = new Person('Coyotiv1', 1)
