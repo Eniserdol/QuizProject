26d2d4b00e08796716c145360d7e42cc8c9c3ba2
Eniserdol <eniserdol@hotmail.com>
2021-01-17 18:11:23 +0300

    Refactor quiz-settings Add performanceLevel

--- src/quiz-settings.js
+++ src/quiz-settings.js
@@ -1,9 +1,24 @@
+// import Persona from './person.js'
+
+// const Person = new Persona()
+
 class QuizSettings {
   constructor(category, difficulty, numberOfQuestions) {
     this.category = category
     this.difficulty = difficulty
     this.numberOfQuestions = numberOfQuestions
   }
-  
+  // this module will decide the user performance level and question difficulty settings
+  /*
+  decideDifficulty() {
+    if (Person.performanceLevel === 1) {
+      this.difficulty = 1
+    } else if (Person.performanceLevel === 2) {
+      this.difficulty = 2
+    }
+    /* else {
+      this.difficulty = 3
+      } */
 }
+
 module.exports = QuizSettings
