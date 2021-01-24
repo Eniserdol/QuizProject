1c53c21a0fd46278702ddfdd6d060bebc990ff1c
Eniserdol <eniserdol@hotmail.com>
2021-01-24 17:25:24 +0300

    Add users

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,10 +1,19 @@
 const express = require('express')
 
 const router = express.Router()
-
+const users = [
+  { name: 'enis', age: 35 },
+  { name: 'nde', age: 5 },
+]
 /* GET users listing. */
 router.get('/', (req, res) => {
-  res.send('respond with a resource')
+  res.send(users)
+})
+
+router.get('/:userId', (req, res) => {
+  const user = users[req.params.userId]
+  if (user) res.render('user', { user })
+  else res.sendStatus(404)
 })
 
 module.exports = router
