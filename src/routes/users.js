58e9fc8bd4015e3c519b9c6303f1dcf75f2548a4
Eniserdol <eniserdol@hotmail.com>
2021-01-29 00:32:53 +0300

    Add create update delete routes to users.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,37 +1,12 @@
+/* eslint-disable no-unused-vars */
 const express = require('express')
 
 const router = express.Router()
 
-// these lines will be refactored
-/*
-const User = require('../models/user')
-const Photo = require('../models/photo')
-*/
 const users = [
   { name: 'enis', age: 35 },
   { name: 'nde', age: 5 },
 ]
-
-/*
-// these lines will be refactored
-const mihri = new User('mihri', 35)
-const armagan = new User('armagan', 36)
-
-const steve = new User('steve', 21)
-steve.bio = 'An awesome hacker who has seen it all, and now sharing them all with you.'
-
-const berlinPhoto = new Photo('berlin.jpg')
-const munichPhoto = new Photo('munich.jpg')
-
-steve.addPhoto(berlinPhoto)
-steve.addPhoto(munichPhoto)
-
-armagan.likePhoto(berlinPhoto)
-mihri.likePhoto(berlinPhoto)
-
-const users = [mihri, armagan, steve]
-
-*/
 
 /* GET users listing. */
 router.get('/', (req, res) => {
@@ -51,4 +26,12 @@ router.get('/:userId', (req, res) => {
   else res.sendStatus(404)
 })
 
+// create a user
+router.post('/users', (req, res) => {})
+
+// update a user
+router.patch('/quizes/:id', (req, res) => {})
+
+// delete user
+router.delete('/quiz/:id', (req, res) => {})
 module.exports = router
