230de27cbf15f2d9cfb03691bcb64e89f877712a
Eniserdol <eniserdol@hotmail.com>
2021-02-01 02:09:57 +0300

    Refactor routes/questions.js

--- src/routes/questions.js
+++ src/routes/questions.js
@@ -1,21 +1,28 @@
 /* eslint-disable no-unused-vars */
 const express = require('express')
+const controller = require('../models/main')
 
 const router = express.Router()
 
 // get all quiz questions
-router.get('/questions', (req, res) => {})
+router.get('/', (req, res) => {})
 
 // get one quiz question
-router.get('/questions/:id', (req, res) => {})
+router.get('/:id', (req, res) => {
+  const { id } = req.params
+  res.send(controller.getQuestionById(id))
+})
 
 // create one quiz question
-router.post('/questions', (req, res) => {})
+router.post('/', (req, res) => {})
 
 // update one quiz question fully
-router.put('/questions/:id', (req, res) => {})
+router.put('/:id', (req, res) => {})
+
+// update one quiz question partially -  question part answer part or correct ans
+router.patch('/:id', (req, res) => {})
 
 // delete one quiz question
-router.delete('/questions/:id', (req, res) => {})
+router.delete('/:id', (req, res) => {})
 
 module.exports = router
