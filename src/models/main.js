ce1580785369c26bac66489cc82d870752ed4497
Eniserdol <eniserdol@hotmail.com>
2021-02-14 18:09:15 +0300

    Refactor main.js Add new user new quiz and new question

--- src/models/main.js
+++ src/models/main.js
@@ -1,9 +1,10 @@
-const QuestionsClass = require('./questions')
-const QuizzesClass = require('./quizzes')
-// const UsersClass = require('./users')
+/*
+const QuestionClass = require('./question')
+const QuizClass = require('./quiz')
+const UserClass = require('./user')
 
-const Questions = new QuestionsClass()
-const Quizzes = new QuizzesClass()
+// const Questions = new QuestionsClass()
+const Quiz = new QuizClass()
 // const Users = new UsersClass()
 
 const getQuestionById = id => {
@@ -30,12 +31,50 @@ const getQuestionsByDifficulty = level => {
 /* const getUsersById = id => {
   return Users.getUser(id)
 }
-*/
+
 module.exports = {
-  getQuestionById,
+ // getQuestionById,
   getQuizzesById,
   getQuizzesAll,
   getQuizzesByDifficulty,
   getQuestionsAll,
   getQuestionsByDifficulty,
 }
+*/
+const mongoose = require('mongoose')
+
+const User = mongoose.model('User', { id: Number, name: String, level: String, quizResults: [] })
+const enis = new User({ id: 1, name: 'enis', level: 'easy', quizResults: [] })
+enis.save().then(() => console.log('we have a new user'))
+
+const Quiz = mongoose.model('Quiz', { id: Number, difficulty: String, questions: [], name: String })
+const quizOne = new Quiz({ id: 1, difficulty: 'easy', questions: [], name: 'Quiz 1 Easy' })
+const quizTwo = new Quiz({ id: 2, difficulty: 'easy', questions: [], name: 'Quiz 2 Easy' })
+const quizThree = new Quiz({ id: 3, difficulty: 'easy', questions: [], name: 'Quiz 3 Easy' })
+quizOne.save().then(() => console.log('we have a new quiz'))
+quizTwo.save().then(() => console.log('we have a new quiz'))
+quizThree.save().then(() => console.log('we have a new quiz'))
+
+const Question = mongoose.model('Question', {
+  id: Number,
+  difficulty: String,
+  text: String,
+  choices: [],
+  correctAnswer: String,
+})
+const questionOne = new Question({
+  id: 1,
+  difficulty: 'easy',
+  text: 'What is 2+2',
+  choices: ['4', '5', '6'],
+  correctAnswer: '4',
+})
+const questionTwo = new Question({
+  id: 2,
+  difficulty: 'hard',
+  text: 'What is 20+20',
+  choices: ['40', '50', '60'],
+  correctAnswer: '40',
+})
+questionOne.save()
+questionTwo.save()
