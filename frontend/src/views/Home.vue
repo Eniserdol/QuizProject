eaec4f9c03cedd3438a8cb774aa2343504ef7291
Eniserdol <eniserdol@hotmail.com>
2021-03-14 18:12:02 +0300

    Add and Create frontend via vue

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  .home
+    img(alt='Vue logo' src='../assets/logo.png')
+    HelloWorld(msg='Welcome to Your Vue.js App')
+    div(v-for="quiz in quizzes") {{quiz.name}}
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      quizzes: []
+    }
+  },
+  async created() {
+    const quizRequest = await axios.get('/api/quiz')
+    this.quiz = quizRequest.data
+  }
+}
+</script>
