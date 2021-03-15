ffa3c58804c45dd945797767b03b719fe237882d
Eniserdol <eniserdol@hotmail.com>
2021-03-16 00:35:00 +0300

    Delete HelloWorld Create quiz-card

---
+++ frontend/src/components/quiz-card.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: 'QuizCard',
+  props: ['quiz'],
+  data() {}
+}
+</script>
+
+<template lang="pug">
+.box
+  h2  {{ quiz.name }} {{quiz.difficulty}}
+</template>
+
+<style lang="scss" scoped>
+.box {
+  padding: 2rem;
+  border: 1px solid rgb(51, 51, 51);
+  background: rgb(194, 172, 194);
+  border-radius: 0.3rem;
+}
+</style>
