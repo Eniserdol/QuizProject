4db6916973b830500843e828eb0727c4a001eec9
Eniserdol <eniserdol@hotmail.com>
2021-03-16 00:33:46 +0300

    Refactor document names

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,27 +1,36 @@
-<template lang="pug">
-  .home
-    img(alt='Vue logo' src='../assets/logo.png')
-    HelloWorld(msg='Welcome to Your Vue.js App')
-    div(v-for="quiz in quizzes") {{quiz.name}}
-</template>
-
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 import axios from 'axios'
+import QuizCard from '@/components/quiz-card.vue'
+
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    QuizCard
   },
   data() {
     return {
-      quizzes: []
+      quizzes: [],
+      users: [],
+      questions: []
     }
   },
   async created() {
-    const quizRequest = await axios.get('/api/quiz')
-    this.quiz = quizRequest.data
+    const quizRequest = await axios.get('/api/quizzes')
+    // const userRequest = await axios.get('/api/users')
+    // const questionRequest = await axios.get('/api/questions')
+    this.quizzes = quizRequest.data
+    // this.questions = questionRequest.data
+    // this.users = userRequest.data
   }
 }
 </script>
+<template lang="pug">
+  .home
+    h1 Welcome please choose your quiz
+    quiz-card(v-for="quiz in quizzes" :quiz="quiz")
+
+    //- div(v-for="quiz in quizzes") {{quiz.name}} has {{quiz.difficulty}}
+    //- div(v-for="user in users") {{user.name}}
+    //- div(v-for="question in questions") {{question.text}}
+</template>
