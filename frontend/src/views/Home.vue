d52d4cb46e1899a565562d73928a6194746b2158
Eniserdol <eniserdol@hotmail.com>
2021-03-21 18:54:08 +0300

    Refactor views-Home

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,9 +1,9 @@
 <template lang="pug">
   .home
-    
+
     h1 Welcome please click start
     #nav
-      router-link(to="/quiz") Start
+      router-link(to="/quizzes") Start
     router-view
 
 </template>
