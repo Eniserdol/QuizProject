ec19f74c74e0e7974e93f74dd9c2698fd4642bfe
Eniserdol <eniserdol@hotmail.com>
2021-03-21 18:52:53 +0300

    Add question-card.vue

---
+++ frontend/src/components/question-card.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  name: 'QuestionCard',
+  props: ['question'],
+  data() {
+    return {}
+  }
+}
+</script>
+<template lang="pug">
+.box
+  h2 Question: {{ question.text}}
+  .box
+    h2 Choices: {{question.choices}}
+
+</template>
+
+<style lang="scss" scoped>
+.box {
+  padding: 2rem;
+  border: 1px solid rgb(51, 51, 51);
+  background: rgb(194, 172, 194);
+  border-radius: 0.3rem;
+}
+</style>
