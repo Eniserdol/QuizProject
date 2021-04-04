51ac44eebba7e86586cac7a56b55a5f6c631fe74
Eniserdol <eniserdol@hotmail.com>
2021-04-04 17:36:41 +0300

    Refactor questionShema for api use

--- backend/src/models/question.js
+++ backend/src/models/question.js
@@ -1,22 +1,13 @@
+const { text } = require('express')
 const mongoose = require('mongoose')
 
 const questionSchema = new mongoose.Schema({
   difficulty: String,
+  category: String,
+  type: String,
   text: String,
   choices: [],
-  correctAnswer: String,
+  correct_answer: String,
 })
 module.exports = mongoose.model('Question', questionSchema)
-/*
-class Question {
-  constructor(id, difficulty, text, choices, correctAnswer) {
-    this.id = id
-    this.difficulty = difficulty
-    this.text = text
-    this.choices = choices
-    this.correctAnswer = correctAnswer
-  }
-}
 
-module.exports = Question
-*/
