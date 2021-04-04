21ce44620f2c0058aa97e7af89d88c036d454b0c
Eniserdol <eniserdol@hotmail.com>
2021-04-04 17:37:41 +0300

    Refactor home view

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,9 +1,11 @@
 <template lang="pug">
   .home
 
-    h1 Welcome please click start
+    h1 Welcome
+    h2 please click Start for defined quizzes or define your quiz
     #nav
       router-link(to="/quizzes") Start
     router-view
+  body
 
 </template>
