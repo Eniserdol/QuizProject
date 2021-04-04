ef250fdfa974ae1ed9c1421d45015c96b7457db4
Eniserdol <eniserdol@hotmail.com>
2021-04-04 17:35:57 +0300

    Remove hard coded questions Add api questions to backend

--- backend/src/models/main.js
+++ backend/src/models/main.js
@@ -1,142 +1,72 @@
 /* eslint-disable no-console */
 
 // const mongoose = require('mongoose')
+// eslint-disable-next-line no-unused-vars
+const axios = require('axios')
 const User = require('./user')
 const Quiz = require('./quiz')
 const Question = require('./question')
-
-// create user
-// const enis = new User({ name: 'enis', level: 'easy', quizResults: [] })
-// enis.save()
-
-// async function main() {
-// const nde = await User.create({  name: 'nde', level: 'easy', quizResults: [] })
-// nde.save()
-// }
-// main()
-
-// create quiz
-// const quizOne = new Quiz({ difficulty: 'easy', questions: [question.id, question.id, question.id], name: 'Quiz 1' })
-// const quizTwo = new Quiz({
-//   difficulty: 'easy',
-//   questions: [questionFour.id, questionFive.id, questionSix.id],
-//   name: 'Quiz 2',
-// })
-// const quizThree = new Quiz({
-//   difficulty: 'hard',
-//   questions: [questionSeven, questionEight, questionNine],
-//   name: 'Quiz 3',
-// })
-// quizOne.save()
-// quizTwo.save()
-// quizThree.save()
-
-// create question
-const questions = [
-  {
-    difficulty: 'middle',
-    text: 'What is try docker',
-    choices: ['1', '2', '3'],
-    correctAnswer: '2',
-  },
-  {
-    difficulty: 'easy',
-    text: 'What is 1+1',
-    choices: ['1', '2', '3'],
-    correctAnswer: '2',
-  },
-  {
-    difficulty: 'easy',
-    text: 'What is 2+2',
-    choices: ['4', '5', '6'],
-    correctAnswer: '4',
-  },
-  {
-    difficulty: 'easy',
-    text: 'What is 3+3',
-    choices: ['3', '5', '6'],
-    correctAnswer: '6',
-  },
-  {
-    difficulty: 'easy',
-    text: 'What is 4+4',
-    choices: ['4', '8', '10'],
-    correctAnswer: '8',
-  },
-  {
-    difficulty: 'easy',
-    text: 'What is 5+5',
-    choices: ['40', '50', '60'],
-    correctAnswer: '40',
-  },
-  {
-    difficulty: 'easy',
-    text: 'What is 6+6',
-    choices: ['12', '15', '20'],
-    correctAnswer: '12',
-  },
-  {
-    difficulty: 'hard',
-    text: 'What is 7+7',
-    choices: ['14', '15', '16'],
-    correctAnswer: '14',
-  },
-  {
-    difficulty: 'hard',
-    text: 'What is 8+8',
-    choices: ['14', '15', '16'],
-    correctAnswer: '16',
-  },
-  {
-    difficulty: 'hard',
-    text: 'What is 9+9',
-    choices: ['16', '18', '20'],
-    correctAnswer: '18',
-  },
-]
 
 // create user
 const users = [
   { name: 'enis', level: 'easy', quizResults: [] },
   { name: 'nde', level: 'easy', quizResults: [] },
 ]
+const answerArray = results => {
+  return results.map(question => {
+    const choices = question.incorrect_answers.concat(question.correct_answer)
+    return {
+      text: question.question,
+      choices,
+      correct_answer: question.correct_answer,
+      difficulty: question.difficulty,
+      type: question.type,
+      category: question.category,
+    }
+  })
+}
 
 async function anything() {
   await Question.remove({})
   await Quiz.remove({})
-  await User.remove({})
+  // await User.remove({})
 
-  // questions.forEach(q => Question.create(q))
-  // for (const question of questions) {
-  //   await Question.create(question)
-  // }
-  const createdQuestions = await Question.create(questions)
+   const questionRequest1 = await axios.get('https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple')
+
+  const questionRequest2 = await axios.get('https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple')
+
+  const questionRequest3 = await axios.get('https://opentdb.com/api.php?amount=10&difficulty=hard&type=multiple')
+
+  const createdQuestions = await Question.create(
+    answerArray([].concat(questionRequest1.data.results, questionRequest2.data.results, questionRequest3.data.results))
+  )
+
+  console.log(createdQuestions)
   // create quiz
   const quizzes = [
     {
-      difficulty: 'easy',
-      questions: [createdQuestions[0], createdQuestions[1], createdQuestions[2]],
+      difficulty: 'Easy',
+      questions: createdQuestions.filter(q => {
+        return q.difficulty == 'easy'
+      }),
       name: 'Quiz1',
     },
     {
-      difficulty: 'easy',
-      questions: [createdQuestions[3], createdQuestions[4], createdQuestions[5]],
+      difficulty: 'Medium',
+      questions: createdQuestions.filter(q => {
+        return q.difficulty == 'medium'
+      }),
       name: 'Quiz2',
     },
     {
-      difficulty: 'hard',
-      questions: [createdQuestions[6], createdQuestions[7], createdQuestions[8]],
+      difficulty: 'Hard',
+      questions: createdQuestions.filter(q => {
+        return q.difficulty == 'hard'
+      }),
       name: 'Quiz3',
     },
   ]
   await Quiz.create(quizzes)
   await User.create(users)
-
-  const createdQuestion = await Question.find({})
-  console.log(createdQuestion)
-  const createdQuiz = await Quiz.find({})
-  console.log(createdQuiz)
-  const createdUser = await User.find({})
-  console.log(createdUser)
 }
 anything()
