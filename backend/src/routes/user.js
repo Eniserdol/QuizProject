d400f2a24f3ead8ea510d1b493890a30be24917b
Eniserdol <eniserdol@hotmail.com>
2021-04-05 23:55:06 +0300

    Refactor routes with class example

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 const express = require('express')
+const axios = require('axios')
 
 const router = express.Router()
 
@@ -14,38 +15,51 @@ router.get('/', async (req, res) => {
       query[key] = req.query[key]
     }
   })
-
-  // if (req.query.name) {
-  //   query.name = req.query.name
-  // }
-  // if (req.query.level) {
-  //   query.level = req.query.level
-  // }
-  // if (req.query.quizResults) {
-  //   query.quizResults = req.query.quizResults
-  // }
   let users = await User.find(query)
   users = users.map(user => {
     return {
       name: user.name,
+      age: user.age,
       level: user.level,
     }
   })
   // res.render('user-list', { users })
-  res.send(users )
-})
-
-router.get('/:id', async (req, res) => {
-  const user = await User.findById(req.params.id)
-  if (user) res.render('user-detail', { user: user.name, level: user.level })
-  else res.sendStatus(404)
+  res.send(users)
 })
 
 // create a user
 router.post('/', async (req, res) => {
-  const createdUser = await User.create(req.body)
-  console.log(createdUser, 'stop')
+  const userToCreate = {
+    name: req.body.name,
+    age: req.body.age,
+  }
+
+  const createdUser = await User.create(userToCreate)
   res.send(createdUser)
+})
+
+// router.get('/:id', async (req, res) => {
+//   const user = await User.findById(req.params.id)
+//   if (user) res.render('user-detail', { user: user.name, level: user.level })
+//   else res.sendStatus(404)
+// })
+
+router.get('/initialize', async (req, res) => {
+  const mihri = new User({ name: 'mihri', age: 35, email: 'mihri@coyotiv.com' })
+  await mihri.setPassword('test')
+  await mihri.save()
+
+  const armagan = new User({ name: 'armagan', age: 36, email: 'armagan@coyotiv.com' })
+  await armagan.setPassword('test')
+  await armagan.save()
+
+  const steve = new User({ name: 'steve', age: 21, email: 'steve@coyotiv.com' })
+  await steve.setPassword('test')
+  await steve.save()
+
+  steve.bio = 'An awesome hacker who has seen it all, and now sharing them all with you.'
+  steve.save()
+  res.sendStatus(200)
 })
 
 // update a user
