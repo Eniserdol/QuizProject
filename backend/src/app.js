a68424154cbbc527229348659608f0ae14035ccd
Eniserdol <eniserdol@hotmail.com>
2021-04-05 23:52:40 +0300

    Refactor app.js Add session and passport

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,16 +3,21 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
-
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
 require('dotenv').config()
+const passport= require('passport')
 
-require('./database-connection')
 
+const mongooseConnection = require('./database-connection')
+// require('./database-connection')
 const indexRouter = require('./routes/index')
 const userRouter = require('./routes/user')
 const quizRouter = require('./routes/quiz')
 const questionRouter = require('./routes/question')
+const accountRouter = require('./routes/account')
 
+const  secretp= process.env.SECRET
 const app = express()
 
 if (app.get('env') == 'development') {
@@ -32,9 +37,34 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+
+app.use(
+  session({
+    secret: `${secretp}`,
+    store: new MongoStore({ mongooseConnection, stringify: false }),
+    cookie: {
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      path: '/api',
+    },
+  })
+)
+app.use(passport.initialize());
+app.use(passport.session());
+
+const User = require('./models/user')
+
+passport.use(User.createStrategy())
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
+
+
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/api', indexRouter)
+app.use('/api/account', accountRouter)
 app.use('/api/users', userRouter)
 app.use('/api/quizzes', quizRouter)
 app.use('/api/questions', questionRouter)
@@ -45,15 +75,21 @@ app.use((req, res, next) => {
 })
 
 // error handler
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
   res.status(err.status || 500)
-  res.render('error')
+  
+  res.send({
+    status: err.status,
+    message: err.message,
+    stack: req.app.get('env') == 'development' ? err.stack : '',
+  })
 })
+
 
 console.log('i am alive   ')
 
