f81d9f6ac43c26eb99192349278352d011af5ea4
Eniserdol <eniserdol@hotmail.com>
2021-04-08 18:34:10 +0300

    Refactor backend user model

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -2,13 +2,18 @@ const mongoose = require('mongoose')
 const passportLocalMongoose = require('passport-local-mongoose')
 
 const userSchema = new mongoose.Schema({
-  name: String,
-  // level: String,
-  age: Number,
-  //  quizResults: [],
+  name: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  bio: String,
 })
 userSchema.plugin(passportLocalMongoose, {
   usernameField: 'email',
 })
 module.exports = mongoose.model('User', userSchema)
-
