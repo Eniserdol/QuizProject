90a82ba1cc39887815aeef478d6f9b0234e3cb89
Eniserdol <eniserdol@hotmail.com>
2021-04-08 18:34:43 +0300

    Add login details

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,24 @@
 <script>
-import axios from 'axios'
+import { mapActions } from 'vuex'
+
 export default {
   name: 'App',
-  async created() {
-    console.log((await axios.get('/api/session')).data)
+  methods: {
+    ...mapActions(['logout']),
+    async doLogout() {
+      await this.logout()
+      this.$router.push('/login')
+    }
   }
 }
 </script>
 <template lang="pug">
   #app
+    #nav
+      router-link(to="/profile") Profile
+      router-link(to="/login") Login
+      router-link(to="/register") Register
+      a(@click="doLogout" href="#") Logout
     router-view
 </template>
 
@@ -17,21 +27,22 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
-
 #nav {
   padding: 30px;
 
   a {
+    font-weight: bold;
     color: #2c3e50;
+    margin: 0 1rem;
 
     &.router-link-exact-active {
       color: #42b983;
     }
   }
 }
+
 .container {
   max-width: 1040px;
   margin-left: auto;
@@ -41,7 +52,7 @@ export default {
 .box {
   padding: 2rem;
   border: 1px solid rgb(51, 51, 51);
-  color:#ecf0f1;
+  color: #ecf0f1;
   background: #3498db;
   border-radius: 0.3rem;
 }
@@ -49,14 +60,12 @@ export default {
 .start-btn {
   display: block;
   cursor: pointer;
-  padding:1rem ;
-  background:#1abc9c;
-  color:chartreuse;
+  padding: 1rem;
+  background: #1abc9c;
+  color: chartreuse;
   font-weight: bold;
   border-radius: 12px;
   overflow: hidden;
   text-decoration: none;
-
 }
-
 </style>
