71027caf27e628c50380ce0949151fd98c635b2e
Eniserdol <eniserdol@hotmail.com>
2021-04-24 19:19:58 +0300

    Refactor origin link

--- backend/src/app.js
+++ backend/src/app.js
@@ -32,7 +32,7 @@ app.use(
 
 app.use(
   cors({
-    origin: process.env.NODE_ENV == 'production' ? 'https://backend-rhjopcioxq-ew.a.run.app' : true,
+    origin: process.env.NODE_ENV == 'production' ? 'https://frontend-rhjopcioxq-ew.a.run.app' : true,
     credentials: true,
   })
 )
