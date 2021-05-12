300bd6289ecb4e0aef19bd4f2289e7102a3c56bd
Eniserdol <eniserdol@hotmail.com>
2021-05-12 18:58:54 +0300

    Add results

--- backend/src/routes/quiz.js
+++ backend/src/routes/quiz.js
@@ -33,6 +33,22 @@ router.get('/:id', async (req, res) => {
   else res.sendStatus(404)
 })
 
+router.post('/:id/control', async (req, res) => {
+  const quiz = await Quiz.findById(req.params.id).populate('questions')
+  const ans = {}
+  quiz.questions.map(q => {
+    let a = false
+    if (q.choices[req.body[q._id]] == q.correct_answer) {
+      a = true
+    }
+    ans[q._id] = a
+    return true
+  })
+  console.log(ans)
+  if (quiz) res.send(ans)
+  else res.sendStatus(404)
+})
+
 // create a quiz
 router.post('/', (req, res) => {})
 
